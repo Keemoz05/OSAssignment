8373d7eaac301338fc7df3a9b4afb8d837a116a9
C2Meng <139868860+C2Meng@users.noreply.github.com>
2025-12-28 19:39:57 +0000

    Client now write guess input to server
    On writing the PID and guess to server, tried to sprintf them together, but kept getting segmentation error on the pid (e.g sends 1 to server, but receives -9193123813)
    
    if u kno how, can try to properly packet the write in one sprintf

--- client.c
+++ client.c
@@ -3,13 +3,18 @@
 #include <unistd.h>
 #include <string.h>
 #include <fcntl.h>
-
+#include <sys/stat.h>
+#include <signal.h>
 
 //NOTE: ALWAYS END PRINTF WITH NEWLINE TO AVOID BUFFER/INPUT HALT ISSUES, THERES A REASON SOMEWHERE BUT IM TOO LAZY TO FIND IT
 #define SIGNAL_GAME_START 1
 #define SIGNAL_YOUR_TURN  2
+#define SIGNAL_GAME_OVER  3
+
 int main(int argc, char *argv[]) {
     // variable declarations //
+    char guess[51];
+
     // 1. Get my ID from the arguments passed by Server
     if (argc < 2) {
         printf("Error: Run this via the server!\n");
@@ -24,7 +29,9 @@ int main(int argc, char *argv[]) {
     char player_name[100];
 
     // open named pipe file for reading and writing //
-    fd = open("player_pipe", O_RDWR);
+    //fd = open("player_pipe", O_RDWR);
+
+   
 
     // enter your name //
     printf("Enter your name:");
@@ -36,7 +43,18 @@ int main(int argc, char *argv[]) {
     //player_name[strcspn(player_name, "\n")] = 0;
 
     // write player name to pipe //
+    fd = open("player_pipe", O_WRONLY);
+
+      // error handling if pipe fails to open //
+    if ( fd < 0){
+        perror("Failed to open pipe");
+        exit(1);
+    }
+
     write(fd, player_name, strlen(player_name));
+    printf(player_name, " sent!");
+
+    close(fd);
 
     // close the pipe //
 
@@ -50,7 +68,7 @@ int main(int argc, char *argv[]) {
 
     // IMPORTANT: Client opens this as RDONLY (Read Only)
     int my_mailbox = open(my_pipe_name, O_RDONLY);
- while(1) {
+    while(1) {
         int signal;
         int n = read(my_mailbox, &signal, sizeof(int));
         if (n > 0) {
@@ -76,7 +94,33 @@ int main(int argc, char *argv[]) {
 
                 case SIGNAL_YOUR_TURN:
                     printf("\n[IT IS YOUR TURN]\n");
-                    printf("Enter your guess:\n ");
+                    printf("Enter your guess:\n");
+                    fgets(guess, sizeof(guess), stdin);
+
+                    printf(my_id,"\n");
+                    printf("opening\n");
+
+                    fd = open("player_pipe", O_WRONLY);
+                      // error handling if pipe fails to open //
+                    if ( fd < 0){
+                        perror("Failed to open pipe");
+                        exit(1);
+                    }
+
+                    //send id
+                    int id_to_send = atoi(my_id);
+                    write(fd, &id_to_send, sizeof(int));
+
+                    //send guess
+                    write(fd, guess, strlen(guess));
+                    
+                    printf(" sent guess: %s packet!\n", guess);
+
+                    
+                    close(fd);
+                
+
+                    //printf(guess,"\n");
                     
                     // char guess_buffer[100];
                     // fgets(guess_buffer, sizeof(guess_buffer), stdin);
